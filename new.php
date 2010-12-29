ac3810996055546910ac7d0d58ba392c311b28fb
Francesco Tassi <ftassi@MacBook-Pro-di-Francesco-Tassi.local>
2010-12-29 23:33:41 +0100

    corretta indentazione

--- new.php
+++ new.php
@@ -1,24 +1,24 @@
 <?php
 include_once('config.php');
-try 
+try
 {
-$form = new ContactForm();
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-	$form->bind($_POST);
-	$form->setValidators(array('firstname', 'lastname', 'phone'));	
-	if($form->isValid())
-  {
-		$sql = "INSERT INTO contacts (firstname, lastname, phone, mobile) VALUES ('%s', '%s', '%s', '%s')";
-		$db->execute($sql, $form['firstname'], $form['lastname'], $form['phone'], $form['mobile']);
-    header('Location: index.php');
-    
-  }
-}
+	$form = new ContactForm();
+	if($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		$form->bind($_POST);
+		$form->setValidators(array('firstname', 'lastname', 'phone'));
+		if($form->isValid())
+		{
+			$sql = "INSERT INTO contacts (firstname, lastname, phone, mobile) VALUES ('%s', '%s', '%s', '%s')";
+			$db->execute($sql, $form['firstname'], $form['lastname'], $form['phone'], $form['mobile']);
+			header('Location: index.php');
+
+		}
+	}
 }
 catch(MySqlException $e)
 {
-  die($e->getMessage());
+	die($e->getMessage());
 }
 catch(Exception $e)
 {
